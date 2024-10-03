7b3de28373a350442506391e5d3d0920f2fb3a12
MakirovicKH <mahirrx-ab205@code.edu.az>
2024-10-03 16:54:36 +0400

    create arrays

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] num = { 2, 3, 4, 4, 54, 3, 75, 22, 99 };
+//foreach (var item in num)
+//{
+//    if (item % 2 == 0)
+//    {
+//        Console.WriteLine("even");
+//    }
+//    else
+//    {
+//        Console.WriteLine("odd");
+//    }
+
+//}
+
+for (int i = 0; i < num.Length; i++)
+
+{
+    if (num[i] % 2 == 0)
+    {
+        Console.WriteLine("even "  + i);      
+    }else
+        Console.WriteLine("odd " + i);
+} 
